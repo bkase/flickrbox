742e14b1c960ac40471c40d3b1e5dc3fe3dcf45f
Brandon Kase <bkase@cmu.edu>
2013-05-27 18:13:06 -0700

    Encoder skeleton using streams

--- encoder.js
+++ encoder.js
@@ -2,9 +2,21 @@ var compress = null;
 var steno = null;
 var rgbify = null;
 var pngify = null;
+var Q = require('q');
 var _ = require('lodash');
 
-module.exports = function(data, randomImage) {
-  return pngify(compress(data), rgbify(randomImage));
+// returns a stream of a png image
+module.exports = function(fileStream, randomImage) {
+  var deferred = Q.defer();
+  rgbify(randomImage, function() {
+    deferred.resolve(
+      fileStream
+          .pipe(compress)
+          .pipe(steno(randomImage))
+          .pipe(pngify)
+    );
+  });
+
+  return deferred.promise();
 };
 
