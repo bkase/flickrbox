381754d43aac0e6053117694acf35420abb359ba
Brandon Kase <bkase@cmu.edu>
2013-05-27 18:44:04 -0700

    Encoder skeleton refactor

--- encoder.js
+++ encoder.js
@@ -1,22 +1,22 @@
-var compress = null;
+var compress = require('./compress.js');
 var steno = null;
-var rgbify = null;
-var pngify = null;
+var PNG = require('pngjs').PNG;
 var Q = require('q');
 var _ = require('lodash');
 
 // returns a stream of a png image
 module.exports = function(fileStream, randomImage) {
   var deferred = Q.defer();
-  rgbify(randomImage, function() {
-    deferred.resolve(
+  randomImage
+    .pipe(new PNG())
+    .on('parsed', function() {
       fileStream
-          .pipe(compress)
-          .pipe(steno(randomImage))
-          .pipe(pngify)
-    );
-  });
-
-  return deferred.promise();
+        .pipe(compress())
+        .pipe(steno(this.data))
+        .on('stenographed', function() {
+          deferred.resolve(this.pack());
+        }.bind(this));
+    });
+  return deferred.promise;
 };
 
